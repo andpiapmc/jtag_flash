a49ad946c86e71bd73cd33515b2f6763de2b94d2
andrea.piattino <andrea.piattino@phase.eu>
2026-07-13 14:59:38 +0200

    Function to list FTDI devices

--- jtag_flash.py
+++ jtag_flash.py
@@ -2,19 +2,44 @@
 #               JTAG management tool                    #
 #########################################################
 
+import ftd2xx as ftd
 
+
+# Menu list
+menu_list = [
+    "0. Exit",
+    "1. List FTDI devices"
+]
+
+
+# Function to list FTDI devices
+def list_ftdi_devices():
+    print("Scan for FTDI devices in progress...")
+    
+    devices = ftd.listDevices()
+    
+    if devices is None:
+        print("No FTDI devices detected.")
+        return
+
+    print(f"Found {len(devices)} FTDI endpoints:")
+    
+    for i, dev in enumerate(devices):
+        dev_name = dev.decode('utf-8', errors='ignore')
+        print(f"Index {i}: {dev_name}")
+        
+
+# Menu display function
 def show_menu():
-    """
-    Display a simple menu for the user to choose an action
-    """
     print("\n" + "-" * 40)
 
-    # menu list
-    print("0. Exit")
+    for item in menu_list:
+        print(item)
 
     print("-" * 40 + "\n")
 
 
+# Main execution block
 if __name__ == '__main__':
     # Main loop
     while True:
@@ -26,6 +51,10 @@ if __name__ == '__main__':
             case "0":
                 print("Exiting...\n")
                 break
+
+            case "1":
+                list_ftdi_devices()
+                
             case _:
                 pass
 
